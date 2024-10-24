ff4fabe57d73732f781a203bc9031637ffb74e11
nasaro <nasaro@gmail.com>
2024-10-24 13:05:46 +0200

    2024-10-24

--- procesamiento.php
+++ procesamiento.php
@@ -13,7 +13,7 @@
             color: #fff;
             background-image: url('aaa.jpg');
         }
-        div{
+        div{     
             width: 70%;
             margin: auto;
         }
@@ -86,8 +86,9 @@
         if (isset($_POST["enviar"])) {
             echo "<table>";
             echo "<tr><th>Texto</th><th>Categorías</th></tr>";
-
+            
             foreach ($_POST as $txt ) {
+                $cat10=false;
                 if ($txt != "Enviar") {
                     if (strcmp($txt, " ") == 0 || strcmp($txt, "") == 0) {
                         echo "<tr><td>Cadena vacía</td><td>1</td></tr>";
@@ -95,39 +96,107 @@
                         echo "<tr><td>$txt</td><td>";
                         if (preg_match("'^\s*[a-zA-Z]+\s*$'", $txt)) {
                             echo "2";
+                            $cat10=true;
                         }
-                        if (preg_match("'^\s*[a-zA-Z]+\s*[a-zA-Z]*$'", $txt)) {
+                        if (preg_match("'\s*[a-zA-Z]+\s+[a-zA-Z]*'", $txt)) {
                             echo " 3";
+                            $cat10=true;
                         }
-                        if (preg_match("'^\s*([A-Za-z\s]+,*)+$'", $txt)) {
+                        if (preg_match("'\s*([A-Za-z]+,){2,}([A-Za-z]*)'", $txt)) {
                             echo " 4";
+                            $cat10=true;
                         }
-                        if (preg_match("'^[A-Za-z]+(\s?){1,}[0-9]+,[0-9]$'", $txt)) {
+                        if (preg_match("'([A-Za-z]?)+(\s?){1,}[0-9]+,[0-9]+'", $txt)) {
                             echo " 5";
+                            $cat10=true;
                         }
-                        if (preg_match("'^[A-Za-z]+(\s?)[13579]{1}$'",$txt)) {
+                        if (preg_match("'([0-9])*[13579]{1}'",$txt)) {
                             echo " 6";
+                            $cat10=true;
                         }
                         if (preg_match("'\+[0-9]{2}\s?[6789][0-9]{8}$'",$txt)) {
                             echo " 7";
+                            $cat10=true;
                         }
                         if (preg_match("'[0-9]{8}[A-Z]{1}'",$txt)) {
                             echo " 8";
+                            $cat10=true;
+                        }
+                        if (preg_match("'^(?=.*[0-9].*[0-9])(?=.*[A-Z])(?=.*[^A-Za-z0-9].*[^A-Za-z0-9].*[^A-Za-z0-9]).{8,20}$'",$txt)) {
+
+                            $cat10=true;
+                        }
+                        if (!$cat10) {
+                            echo " 10";
                         }
                         echo "</td></tr>";
                     }
                 }
             }
             echo "</table>";
+            ?>
+            <div>
+                <table>
+                    <caption>Validación de Cadenas</caption>
+                    <tbody>
+                        <tr>
+                            <td>1.</td>
+                            <td>Cadena vacía.</td>
+                        </tr>
+                        <tr>
+                            <td>2.</td>
+                            <td>Cadena con una única palabra (sólo letras, puede haber espacios delante y detrás).</td>
+                        </tr>
+                        <tr>
+                            <td>3.</td>
+                            <td>Cadena con dos palabras (sólo letras, separadas por uno o varios espacios).</td>
+                        </tr>
+                        <tr>
+                            <td>4.</td>
+                            <td>Cadena con una enumeración (tres o más palabras separadas con comas).</td>
+                        </tr>
+                        <tr>
+                            <td>5.</td>
+                            <td>Cadena con un número decimal.</td>
+                        </tr>
+                        <tr>
+                            <td>6.</td>
+                            <td>Cadena con un único número impar.</td>
+                        </tr>
+                        <tr>
+                            <td>7.</td>
+                            <td>Número de teléfono (9 cifras, empezando por 6, 7, 8 o 9 y prefijo, signo + y 2 números).</td>
+                        </tr>
+                        <tr>
+                            <td>8.</td>
+                            <td>Número del DNI (8 números, con letra final mayúscula).</td>
+                        </tr>
+                        <tr>
+                            <td>9.</td>
+                            <td>Contraseña (al menos seis caracteres, debe contener):</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td style="text-align: start">
+                                a. Debe tener entre 8 y 20 caracteres.<br>
+                                b. Debe contener al menos 2 números (no tienen que ser consecutivos).<br>
+                                c. Debe contener al menos 1 letra mayúscula y 3 caracteres especiales (no consecutivos).
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <?php
         }else{
-        
+            
         ?>
     </div>
     
         <form id="myForm"  action="#" method="post" enctype="multipart/form-data">
         <div class="form-group">
             <label for="text1">Texto 1:</label>
-            <input type="text" id="text1" name="text1">
+            <input type="text" id="text1" name="text1"><input type="radio" name="" id="">
         </div>
         <div class="form-group">
             <label for="text2">Texto 2:</label>
@@ -157,6 +226,7 @@
             <label for="image">Selecciona una imagen:</label>
             <input type="file" id="image" name="image" >
         </div>
+
         <input name="enviar" id="boton" value="Enviar" type="submit">
     </form>
     <?php
